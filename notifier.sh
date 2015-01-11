9b3151277deaba857c782c8e99051f55644b880e
MalcolmK <malcolm.k.x@gmail.com>
2015-01-11 15:41:59 +0100

    Back to basic

--- notifier.sh
+++ notifier.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Crontab interval values
-crontab_interval_minute="*"          # Every 15 minutes
-crontab_interval_hour="*"               # Every hour
-crontab_interval_day_of_month="*"       # Every day of the month
-crontab_interval_month="*"              # Every month
-crontab_interval_day_of_week="*"        # Every day
+# crontab_interval_minute="*"          # Every 15 minutes
+# crontab_interval_hour="*"               # Every hour
+# crontab_interval_day_of_month="*"       # Every day of the month
+# crontab_interval_month="*"              # Every month
+# crontab_interval_day_of_week="*"        # Every day
 
 # Functions
 function getHeadRevision () {
@@ -22,27 +22,28 @@ function getWorkingCopyRevision () {
     eval $__resultvar=$(svn info $local_working_copy | grep Revision | cut -d' ' -f2)
 }
 
-function add () {
-    # Get parameters
-    local __wcLocation=$1
-    local __wcMessage=$2
+# function add () {
+#     # Get parameters
+#     local __wcLocation=$1
+#     local __wcMessage=$2
 
-    # Create a new crontab
-    crontab -l > svn_notifier_crontab
+#     # Create a new crontab
+#     crontab -l > svn_notifier_crontab
 
-    # Echo new cron into cron file
-    echo "$crontab_interval_minute  $crontab_interval_hour  $crontab_interval_day_of_month  $crontab_interval_month     $crontab_interval_day_of_week   svn-notifier-wc $local_working_copy $__wcMessage" >> svn_notifier_crontab
+#     # Echo new cron into cron file
+#     echo "$crontab_interval_minute  $crontab_interval_hour  $crontab_interval_day_of_month  $crontab_interval_month     $crontab_interval_day_of_week   svn-notifier-wc $local_working_copy $__wcMessage" >> svn_notifier_crontab
 
-    # Install new cron file
-    crontab svn_notifier_crontab
+#     # Install new cron file
+#     crontab svn_notifier_crontab
 
-    # Remove temporary cron file
-    rm svn_notifier_crontab
+#     # Remove temporary cron file
+#     rm svn_notifier_crontab
 
-    exit
-}
+#     exit
+# }
 
 function notify () {
+    # Get parameters
     getHeadRevision __headRevision
     getWorkingCopyRevision __wcRevision
 
@@ -52,13 +53,18 @@ function notify () {
     fi
 }
 
-if [ "$1" = "add" ]
-    then
-        local_working_copy=$2
-        __wcMessage=$3
-        add $local_working_copy $__wcMessage
-    else
-        local_working_copy=$1
-        wcMessage=$2
-        notify
-fi
+# if [ "$1" = "add" ]
+#     then
+#         local_working_copy=$2
+#         __wcMessage=$3
+#         add $local_working_copy $__wcMessage
+#     else
+#         local_working_copy=$1
+#         wcMessage=$2
+#         notify
+# fi
+
+local_working_copy=$1
+wcMessage=$2
+
+notify
